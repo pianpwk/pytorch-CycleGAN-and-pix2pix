4b3a4e7aae21914a21eaa70f25b4b8f783e83ab2
visenze <visenze@visenzes-MacBook-Pro-8.local>
2020-02-26 13:08:50 +0800

    models

--- models/__init__.py
+++ models/__init__.py
@@ -1,28 +1,44 @@
-def create_model(opt):
+import importlib
+from models.base_model import BaseModel
+
+
+def find_model_using_name(model_name):
+    """Import the module "models/[model_name]_model.py".
+    In the file, the class called DatasetNameModel() will
+    be instantiated. It has to be a subclass of BaseModel,
+    and it is case-insensitive.
+    """
+    model_filename = "models." + model_name + "_model"
+    modellib = importlib.import_module(model_filename)
     model = None
-    if opt.model == 'cycle_gan':
-        #assert(opt.dataset_mode == 'unaligned')
-        from .cycle_gan_model import CycleGANModel
-        model = CycleGANModel()
-    elif opt.model == 'pix2pix':
-        assert(opt.dataset_mode == 'aligned')
-        from .pix2pix_model import Pix2PixModel
-        model = Pix2PixModel()
-    elif opt.model == 'test':
-        assert(opt.dataset_mode == 'single')
-        from .test_model import TestModel
-        model = TestModel()
-    elif opt.model == 'cycle_gan_with_classification_frozen':
-        from .cycle_gan_model_with_classification_frozen import CycleGANModelWithClassificationFrozen
-        model = CycleGANModelWithClassificationFrozen()
-    elif opt.model == 'cycle_gan_semantic':
-        from .cycle_gan_semantic_model import CycleGANSemanticModel
-        model = CycleGANSemanticModel()
-    elif opt.model == 'cycle_gan_2_25':
-        from .cycle_gan_2_25 import CycleGAN225Model
-        model = CycleGAN225Model()
-    else:
-        raise NotImplementedError('model [%s] not implemented.' % opt.model)
-    model.initialize(opt)
-    print("model [%s] was created" % (model.name()))
+    target_model_name = model_name.replace('_', '') + 'model'
+    for name, cls in modellib.__dict__.items():
+        if name.lower() == target_model_name.lower() \
+           and issubclass(cls, BaseModel):
+            model = cls
+
+    if model is None:
+        print("In %s.py, there should be a subclass of BaseModel with class name that matches %s in lowercase." % (model_filename, target_model_name))
+        exit(0)
+
     return model
+
+
+def get_option_setter(model_name):
+    """Return the static method <modify_commandline_options> of the model class."""
+    model_class = find_model_using_name(model_name)
+    return model_class.modify_commandline_options
+
+
+def create_model(opt):
+    """Create a model given the option.
+    This function warps the class CustomDatasetDataLoader.
+    This is the main interface between this package and 'train.py'/'test.py'
+    Example:
+        >>> from models import create_model
+        >>> model = create_model(opt)
+    """
+    model = find_model_using_name(opt.model)
+    instance = model(opt)
+    print("model [%s] was created" % type(instance).__name__)
+    return instance
